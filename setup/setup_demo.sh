b7ef5119ead8a3feb77533569a4fa00a682e0841
David Stadelmann <david.stadelmann@scs.ch>
2024-11-25 12:41:57 +0100

    Update git attributes

--- setup/setup_demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-sed "s#FIXME#${SCRIPTPATH}#g" "${SCRIPTPATH}/radar-demo.desktop.template" > "${HOME}/.local/share/applications/radar-demo.desktop"
-cp "${SCRIPTPATH}/radar.png" "${HOME}/.local/share/radar.png"
