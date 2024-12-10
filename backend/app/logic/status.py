8d64572e47f9bf26857045d9592c84cd58980979
David Stadelmann <david.stadelmann@scs.ch>
2024-12-10 15:38:31 +0100

    Increase integration window

--- backend/app/logic/status.py
+++ backend/app/logic/status.py
@@ -104,9 +104,9 @@ class Fft1DInfo(HwInfo):
     num_aie_used: int = 1
 
     def reset(self):  # pyright: ignore [reportImplicitOverride]
-        self.frame_rate: NDArray[np.int32] = np.zeros(WIN_SIZE).astype(np.int32)
+        self.frame_rate: NDArray[np.int32] = np.zeros(1000).astype(np.int32)
         self.power: float = 0
-        self.ffts_emulation: NDArray[np.int32] = np.zeros(200).astype(np.int32)
+        self.ffts_emulation: NDArray[np.int32] = np.zeros(1000).astype(np.int32)
 
     def set_ffts_emulation(self, value: int):
         self.ffts_emulation = np.append(self.ffts_emulation, value)
