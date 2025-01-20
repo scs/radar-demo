22b8ae79a3638f6c910cf3d41e7b332a0457b455
David Stadelmann <david.stadelmann@scs.ch>
2025-01-20 10:22:21 +0100

    Disable log output from werkzeug

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,7 @@
+import logging
 from app import app
-from app.logic.state import GlobalState
 
 if __name__ == "__main__":
+    log = logging.getLogger("werkzeug")
+    log.setLevel(logging.ERROR)
     app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
